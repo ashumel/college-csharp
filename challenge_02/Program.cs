c94906cec49f11fd5977b431e825876e13aa2e9e
Andrey <engineer.lab09@gmail.com>
2022-10-05 23:46:40 +0300

    Added lab_06 and challenge_02

---
+++ challenge_02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using static System.Console;
+
+namespace challenge_02
+{
+	internal class Program
+	{
+		private static void Task_3()
+		{
+			string line = ReadLine();
+			Regex findSpaces = new Regex(@"\s{2,}");
+			MatchCollection matchSpaces = findSpaces.Matches(line ?? string.Empty);
+			foreach (Match x in matchSpaces)
+				line = line?.Remove(x.Index, x.Value.Length - 1);
+			WriteLine(line);
+			
+		}
+		public static void Main(string[] args)
+		{
+			Task_3();
+		}
+	}
+}
