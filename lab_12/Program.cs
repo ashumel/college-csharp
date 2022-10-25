42f8ec8c987dbe0b2fa2baec066d11d2101aa006
Andrey <engineer.lab09@gmail.com>
2022-10-25 21:29:54 +0300

    Added lab_12

---
+++ lab_12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace lab_12
+{
+	internal static class Program
+	{
+		private static void ConsoleClear()
+		{
+			WriteLine("\nPlease, press Enter to continue...");
+			if (ReadKey().Key != ConsoleKey.Enter)
+				System.Environment.Exit(0);
+			Clear();
+		}
+		
+		
+		public static void Main(string[] args)
+		{
+			Task1();
+			ConsoleClear();
+		}
+
+		private static void Task1()
+		{
+			WriteLine("\t-- Task 1 --\t");
+			int x1, y1, x2, y2;
+			WriteLine("-- Length between x and y --");
+			Write("X: ");
+			x1 = int.Parse(ReadLine() ?? string.Empty);
+			Write("Y: ");
+			y1 = int.Parse(ReadLine() ?? string.Empty);
+			WriteLine($"Result: {Distance.Length(x1, y1)}");
+			WriteLine("\n\n-- Length between x1, y1 and x1, y2 --");
+			Write("X1: ");
+			x1 = int.Parse(ReadLine() ?? string.Empty);
+			Write("Y1: ");
+			y1 = int.Parse(ReadLine() ?? string.Empty);
+			Write("X2: ");
+			x2 = int.Parse(ReadLine() ?? string.Empty);
+			Write("Y2: ");
+			y2 = int.Parse(ReadLine() ?? string.Empty);
+			WriteLine($"Result: {Distance.Length(x1, y1, x2, y2)}");
+		}
+	}
+}
---
+++ lab_12/Replace.cs
@@ -0,0 +1,17 @@
+﻿namespace lab_12
+{
+	public static class Replace
+	{
+		public static string CharInString(string str, int index, char symbol) => 
+			str.Remove(index).Insert(index, symbol.ToString());
+
+		public static string CharInString(string str, char oldChar, char newChar) => 
+			str.Replace(oldChar, newChar);
+		
+		public static string CharInString(string str, int index) =>
+			str.Remove(index).Insert(index, " ");
+
+		public static string CharInString(string str, char symbol) =>
+			str.Replace(symbol.ToString(), " ");
+	}
+}
