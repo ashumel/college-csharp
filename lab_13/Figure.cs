6d836053a7b0f1f39c3c1d68b201858721ede860
Andrey <engineer.lab09@gmail.com>
2022-10-27 10:31:17 +0300

    Added lab_13

---
+++ lab_13/Figure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab_13
+{
+	public abstract class Figure
+	{
+		public abstract double Square();
+	}
+
+	public sealed class Foursquare : Figure
+	{
+		private readonly double _side;
+		public Foursquare(double side)
+		{
+			_side = side;
+		}
+		public override double Square() => Math.Pow(_side, 2);
+	}
+
+	public sealed class Circle : Figure
+	{
+		private readonly double _radius;
+		public Circle(double radius)
+		{
+			_radius = radius;
+		}
+		public override double Square() => Math.PI * Math.Pow(_radius, 2);
+	}
+}
