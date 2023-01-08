a0b911a53b831a14629a6b9d62122485f49ed831
Andrey <engineer.lab09@gmail.com>
2023-01-08 05:20:32 +0300

    Added lab_16

---
+++ lab_16/Card.cs
@@ -0,0 +1,67 @@
+﻿namespace lab_16;
+
+public class Card
+{
+	private delegate void WarningInConsole();
+	private event WarningInConsole LimitReached;
+	private event WarningInConsole LimitRedemption;
+	
+	private double _balance;
+	private readonly double _limit;
+	private readonly string _cardholder;
+	
+	public Card(string cardholder, double balance, double limit)
+	{
+		_cardholder = cardholder;
+		_balance = balance;
+		_limit = limit;
+		LimitReached += ShowMessageLimitReached;
+		LimitRedemption += ShowMessageRedemption;
+	}
+
+	private static void ShowMessageLimitReached()
+	{
+		Console.ForegroundColor = ConsoleColor.Magenta;
+		Console.WriteLine("Limit has been reached!");
+		Console.ResetColor();
+	}
+
+	private static void ShowMessageRedemption()
+	{
+		Console.ForegroundColor = ConsoleColor.Green;
+		Console.WriteLine("Good! Limit has been repaid!");
+		Console.ResetColor();
+	}
+
+	public void Show()
+	{
+		Console.WriteLine("--- Card Info ---\n" +
+		                  $"Balance: {_balance}\n" +
+		                  $"Limit: {_limit}\n" +
+		                  $"Cardholder: {_cardholder}");
+	}
+
+	public void Put(double count)
+	{
+		if ((_balance <= _limit) && (_balance + count) > _limit)
+			LimitRedemption?.Invoke();
+		_balance += count;
+		Console.WriteLine($"Account successfully replenished. Balance: {_balance}");
+		
+	}
+
+	public void Pay(double count)
+	{
+		if (_balance - count < 0)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("The operation has been cancelled. There are not enough funds on the card.");
+			Console.ResetColor();
+			return;
+		}
+		if (_balance - count <= _limit)
+			LimitReached?.Invoke();
+		_balance -= count;
+		Console.WriteLine($"Successfully withdrawn from account. Balance: {_balance}");
+	}
+}
---
+++ lab_16/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace lab_16;
+
+public static class Program
+{
+	public static void Main(string[] args)
+	{
+		var card1 = new Card(
+			cardholder: "Vlad Anisimov",
+			balance: 15.5,
+			limit: 5.5);
+
+		while (true)
+		{
+			Console.WriteLine("1. Show card\n" +
+			                  "2. Make pay\n" +
+			                  "3. Put to card\n" +
+			                  "4. Exit"); 
+			var key = Console.ReadKey().Key;
+			switch (key)
+			{
+				case ConsoleKey.D1:
+					card1.Show();
+					break;
+				case ConsoleKey.D2:
+					Console.WriteLine("\nHow much do you want to withdraw?");
+					card1.Pay(double.Parse(Console.ReadLine() ?? string.Empty));
+					break;
+				case ConsoleKey.D3:
+					Console.WriteLine("\nHow much do you want to deposit?");
+					card1.Put(double.Parse(Console.ReadLine() ?? string.Empty));
+					break;
+				case ConsoleKey.D4:
+					return;
+			}
+		}
+	}
+}
