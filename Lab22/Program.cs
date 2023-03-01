0dfa7a9e6e52895b9acdaa02ac86de102fc8cf7d
Andrey <engineer.lab09@gmail.com>
2023-03-02 00:34:43 +0300

    Restructured lab assignment #22

---
+++ Lab22/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace Lab22;
+
+public static class Program
+{
+	public static void Main(string[] args)
+	{
+		Task1();
+		Task2();
+		Task3();
+	}
+
+	private static void Task1()
+	{
+		var arrayList = new ArrayList
+		{
+			"James", "Walker", "game_engineer"
+		};
+
+		foreach (var o in arrayList) 
+			Console.WriteLine(o);
+
+		arrayList[0] = "Alex";
+		arrayList.Remove("game_engineer");
+	}
+
+	private static void Task2()
+	{
+		var numArray = new[] { 1, 3, 7, 10 }
+			.ToList();
+		var strArray = new[] { "Hello", "World" }
+			.ToList();
+		var numMax = numArray.Max();
+		var strMax = strArray.OrderByDescending(s => s.Length).First();
+		Console.WriteLine($"Max number: {numMax} | Max text length: {strMax}");
+	}
+
+	private static void Task3()
+	{
+		var students = new List<Student>
+		{
+			new("Jerry", "Tillmon", 17),
+			new("Robert", "Donovan", 19),
+			new("Victoria", "Simmons", 20),
+			new("Jeffery", "Haynes", 16),
+			new("Kelly", "Harper", 22),
+		};
+		students.Sort();
+		Console.WriteLine(string.Join("\n", students));
+	}
+	
+}
---
+++ Lab22/Student.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab22;
+
+public class Student : IComparable<Student>
+{
+	public string FirstName { get; private set; }
+	public string LastName { get; private set; }
+	public int Age { get; private set; }
+
+	public Student(string firstName, string lastName, int age)
+	{
+		FirstName = firstName;
+		LastName = lastName;
+		Age = age;
+	}
+
+	public int CompareTo(Student? other) => 
+		Age - other!.Age;
+
+	public override string ToString() => 
+		$"First name: {FirstName} | Last name: {LastName} | Age: {Age}";
+}
